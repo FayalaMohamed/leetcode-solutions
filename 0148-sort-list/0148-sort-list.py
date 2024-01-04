0006e42f65993a5d3bf40a9f14eea9fb81b436f3
FayalaMohamed <99082540+FayalaMohamed@users.noreply.github.com>
2024-01-04 22:27:57 +0100

    Time: 606 ms (64.85%), Space: 53.9 MB (32.53%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -28,11 +28,18 @@ class Solution(object):
         return self.merge(l1, l2)
 
     def merge(self, l1, l2):
-        if not l1 or not l2:
-            return l1 or l2
-        if l1.val < l2.val:
-            l1.next = self.merge(l1.next, l2)
-            return l1
-        else:
-            l2.next = self.merge(l1, l2.next)
-            return l2
+        dummy = ListNode(0)
+        current = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+
+        current.next = l1 or l2
+
+        return dummy.next
