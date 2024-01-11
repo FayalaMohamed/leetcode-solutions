0dba049daade5a232dd035c59f7a7152176723d0
FayalaMohamed <99082540+FayalaMohamed@users.noreply.github.com>
2024-01-11 21:25:34 +0100

    Time: 52 ms (91.11%), Space: 14.5 MB (56.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -5,7 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        res = [-1, -1]
+        """res = [-1, -1]
         i = 0
         while i<len(nums):
             if nums[i]==target and res[0]==-1:
@@ -13,5 +13,34 @@ class Solution(object):
             if nums[i]==target and res[0]!=-1 :
                 res[1] = i
             i+=1
-        return res
+        return res"""
         
+        def find_first(nums, target):
+            start = 0
+            end = len(nums) - 1
+            first = -1
+            while start <= end:
+                mid = (start + end) // 2
+                if nums[mid] > target:
+                    end = mid - 1
+                elif nums[mid] == target:
+                    first = mid
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            return first
+        def find_last(nums, target):
+            start = 0
+            end = len(nums) - 1
+            last = -1
+            while start <= end:
+                mid = (start + end) // 2
+                if nums[mid] > target:
+                    end = mid - 1
+                elif nums[mid] == target:
+                    start = mid + 1
+                    last = mid
+                else:
+                    start = mid + 1
+            return last
+        return [find_first(nums, target), find_last(nums, target)]
