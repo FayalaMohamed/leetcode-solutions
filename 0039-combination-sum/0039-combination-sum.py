5dbfbf55962915c091e60310e7220a16575cc303
FayalaMohamed <99082540+FayalaMohamed@users.noreply.github.com>
2024-05-28 17:52:53 +0200

    Time: 55 ms (47.18%), Space: 16.4 MB (98.40%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,35 +1,22 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        nbs = [0]
-
-        def backtracking(i: int):
-            if i >= len(candidates):
+        cur = []
+        def dfs(i, total):
+            nonlocal cur
+            if total == target:
+                res.append(cur.copy())
+                return
+            if i>len(candidates)-1 or total > target:
                 return
             
-            added = False
-
-            # if we can add the element we dadd it and update the first element of nbs 
-            # which is the sum of the elements of the list
-            if nbs[0] + candidates[i] <= target:
-                nbs.append(candidates[i])
-                nbs[0] = nbs[0] + candidates[i]
-                added = True
-
-                # if the sum is equal to the target we add the solution 
-                # or we call backtracking for the same element we added
-                if nbs[0] == target:
-                    res.append(nbs[1:].copy())
-                else:
-                    backtracking(i)
-
-            # if we added an element we remove it 
-            # we call backtracking for the i+1 element
-            if added :
-                nbs.pop()
-                nbs[0] = nbs[0] - candidates[i]
+            cur.append(candidates[i])
+            dfs(i, total + candidates[i])
             
-            backtracking(i+1)
-
-        backtracking(0)
+            cur.pop()
+            dfs(i+1, total)
+            
+        dfs(0,0)
         return res
+    
+    
