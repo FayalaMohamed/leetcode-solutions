4ecebf041010eaf4dc148e646f6f15546a02b8ce
FayalaMohamed <99082540+FayalaMohamed@users.noreply.github.com>
2024-08-05 21:46:54 +0200

    Time: 103 ms (76.89%), Space: 17.1 MB (67.03%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -5,28 +5,33 @@ class Solution(object):
         :rtype: int
         """
         res = 0
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if grid[x][y] == 1:
-                    stack = [(x, y)]
-                    nb = 0
-                    while stack:
-                        i, j = stack.pop()
-                        if grid[i][j] == 0 :
-                            continue
-                        nb += 1
-                        grid[i][j] = 0
-                        if  0 <= i+1 < len(grid) and 0 <= j < len(grid[0]) and grid[i+1][j] == 1 :
-                            stack.append((i+1, j))
-                            
-                        if  0 <= i-1 < len(grid) and 0 <= j < len(grid[0]) and grid[i-1][j] == 1 :
-                            stack.append((i-1, j))
-                        if  0 <= i < len(grid) and 0 <= j+1 < len(grid[0]) and grid[i][j+1] == 1:
-                            stack.append((i, j+1))
-                        if  0 <= i < len(grid) and 0 <= j-1 < len(grid[0]) and grid[i][j-1] == 1 :
-                            stack.append((i, j-1))
+        
+        visited = set()
+        cols, rows = len(grid[0]), len(grid)
+        curr = 0
+        directions = [[1,0], [0,1], [-1,0], [0,-1]]
+        
+        def dfs(i,j):
+            nonlocal curr
+            q = collections.deque()
+            q.append((i, j))
+            visited.add((i, j))
+            
+            while q :
+                row, col = q.pop()
+                curr += 1
+                for dr, dc in directions:
+                    if 0 <= row+dr < rows and 0 <= col+dc < cols and (row+dr, col+dc) not in visited and grid[row+dr][col+dc]:
+                        q.append((row+dr, col+dc))
+                        visited.add((row+dr, col+dc))
+                
+        
+        for i in range(rows):
+            for j in range(cols):
+                if (i, j) not in visited and grid[i][j] == 1 :
+                    curr = 0
+                    dfs(i,j)
+                    res = max(res,curr)
                     
-                    if nb > res : 
-                        res = nb
-
+        
         return res
